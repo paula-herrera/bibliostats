ec92f7a73d101f7d62e8b99201b4b180432ce7d7
Paula Herrera <herrera.paula5@gmail.com>
2021-02-13 13:59:15 -0700

    Refactored db controllers and implemented getAll route

--- database/controllers/index.js
+++ database/controllers/index.js
@@ -1,13 +1,25 @@
 const Book = require('../models/book.js');
 
-const addBook = (book, cb) => {
-  Book.create(book)
-    .then((book)=> {
-      cb(null, book);
-    })
-    .catch(err => {
-      cb(err, null);
-    });
+let queries = {
+  getAllBooks: (cb) => {
+    Book.find({})
+      .then((books)=> {
+        cb(null, books);
+      })
+      .catch(err => {
+        cb(err, null);
+      });
+  },
+
+  addBook: (book, cb) => {
+    Book.create(book)
+      .then((book)=> {
+        cb(null, book);
+      })
+      .catch(err => {
+        cb(err, null);
+      });
+  }
 }
 
-module.exports = addBook
+module.exports = queries;
