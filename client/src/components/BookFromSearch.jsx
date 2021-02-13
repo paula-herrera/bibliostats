05d681e3ea808ebf39c019c145be245abb970ad7
Paula Herrera <herrera.paula5@gmail.com>
2021-02-13 11:37:14 -0700

    Fleshed out search results to display results

---
+++ client/src/components/BookFromSearch.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const BookFromSearch = ({book}) => {
+  return (
+    <div className="bookToAdd">
+      <div className="col-1">
+        <img src={book.volumeInfo.imageLinks.thumbnail}></img>
+        <button>Add To Shelf</button>
+      </div>
+      <div className="col-2">
+        <p>{book.volumeInfo.title}</p>
+        <p>{book.volumeInfo.authors}</p>
+        <p>{book.volumeInfo.publishedDate.substr(0, 4)}</p>
+        <p>{book.volumeInfo.categories}</p>
+        <p>{book.volumeInfo.pageCount} pages</p>
+      </div>
+      <div className="col-3">
+        <p>{book.volumeInfo.description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default BookFromSearch;
--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import BookFromSearch from './BookFromSearch.jsx';
 
 const SearchResults = ({searchResults}) => {
   let view = <></>
   if (searchResults.length == 0) {
     view = <div className="noResults">Search For Books</div>
   } else {
-    view = <div className="results">Books Go Here</div>
+    view =
+      <div className="results">
+
+        {searchResults.map((book, i) =>
+          <BookFromSearch book={book} key={i}/>
+        )}
+
+      </div>
   }
   return (
     <div className="searchResults">
