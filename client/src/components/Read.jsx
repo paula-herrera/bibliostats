d1d620f8c76d3df847c1a2edde3a51cea4985303
Paula Herrera <herrera.paula5@gmail.com>
2021-02-16 16:01:49 -0700

    Added sort by functionality for all pages

--- client/src/components/Read.jsx
+++ client/src/components/Read.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BookCard from './BookCard.jsx';
 
 const Read = ({books, goToBookPage}) => {
-  books.sort(function(a,b) {
-    return new Date(b.dateAdded) - new Date(a.dateAdded);
-  })
+  const [filter, setFilter] = useState('dateAdded');
+
+  if (filter === 'dateAdded') {
+    books.sort(function(a,b) {
+      return new Date(b.dateAdded) - new Date(a.dateAdded);
+    })
+  } else if (filter === 'title') {
+    books.sort(function(a,b) {
+      if(a.title < b.title) {return -1;}
+      if(a.title > b.title) {return 1;}
+      return 0;
+    })
+  } else if (filter === 'dateStarted') {
+    books.sort(function(a,b) {
+      return new Date(b.dateStarted) - new Date(a.dateStarted);
+    })
+  } else if (filter === 'dateFinished') {
+    books.sort(function(a,b) {
+      return new Date(b.dateFinished) - new Date(a.dateFinished);
+    })
+  } else if (filter === 'rating') {
+    books.sort(function(a,b) {
+      if(a.rating > b.rating) {return -1;}
+      if(a.rating < b.rating) {return 1;}
+      return 0;
+    })
+  }
 
   let body = <></>
   if (books.length === 0) {
@@ -28,6 +52,20 @@ const Read = ({books, goToBookPage}) => {
   return (
     <>
       <h1>Read</h1>
+      <div className="sort-by">
+        <label htmlFor="sort-by-select"><p>Sort By</p></label>
+        <select
+          name="sort-by"
+          id="sort-by-select"
+          onChange={() => setFilter(event.target.value)}
+        >
+          <option value="dateAdded">Date Added</option>
+          <option value="title">Title</option>
+          <option value="dateStarted">Date Started</option>
+          <option value="dateFinished">Date Finished</option>
+          <option value="rating">Rating</option>
+        </select>
+      </div>
       <div className="tbr">
         {body}
       </div>
