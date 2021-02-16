3d1d788833d7ad20416139339ba445eecb80e618
Paula Herrera <herrera.paula5@gmail.com>
2021-02-16 15:52:15 -0700

    Added sort by functionality to currently reading

--- client/src/components/DNF.jsx
+++ client/src/components/DNF.jsx
@@ -5,20 +5,30 @@ const DNF = ({books, goToBookPage}) => {
   books.sort(function(a,b) {
     return new Date(b.dateAdded) - new Date(a.dateAdded);
   })
+  let body = <></>
+  if (books.length === 0) {
+    body = <div>
+      <p>
+        No Books Added to This Shelf
+      </p>
+    </div>
+  } else {
+    body = <div className="tbr-books">
+      {books.map((book, i) =>
+        <BookCard
+          book={book}
+          key={i}
+          goToBookPage={goToBookPage}
+        />
+      )}
+    </div>
+  }
 
   return (
     <>
       <h1>Did Not Finish</h1>
       <div className="tbr">
-        <div className="tbr-books">
-          {books.map((book, i) =>
-            <BookCard
-              book={book}
-              key={i}
-              goToBookPage={goToBookPage}
-            />
-          )}
-        </div>
+        {body}
       </div>
     </>
   )
--- client/src/components/TBR.jsx
+++ client/src/components/TBR.jsx
@@ -5,19 +5,31 @@ const TBR = ({books, goToBookPage}) => {
   books.sort(function(a,b) {
     return new Date(b.dateAdded) - new Date(a.dateAdded);
   })
+
+  let body = <></>
+  if (books.length === 0) {
+    body = <div>
+      <p>
+        No Books Added to This Shelf
+      </p>
+    </div>
+  } else {
+    body = <div className="tbr-books">
+      {books.map((book, i) =>
+        <BookCard
+          book={book}
+          key={i}
+          goToBookPage={goToBookPage}
+        />
+      )}
+    </div>
+  }
+
   return (
     <>
       <h1>To Be Read</h1>
       <div className="tbr">
-        <div className="tbr-books">
-          {books.map((book, i) =>
-            <BookCard
-              book={book}
-              key={i}
-              goToBookPage={goToBookPage}
-            />
-          )}
-        </div>
+        {body}
       </div>
     </>
   )
