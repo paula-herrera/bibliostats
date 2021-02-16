8cbfa14b962bec4a3baaa1519179bfb292da04b6
Paula Herrera <herrera.paula5@gmail.com>
2021-02-16 16:01:49 -0700

    Added sort by functionality for all pages

--- client/src/components/DNF.jsx
+++ client/src/components/DNF.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BookCard from './BookCard.jsx';
 
 const DNF = ({books, goToBookPage}) => {
-  books.sort(function(a,b) {
-    return new Date(b.dateAdded) - new Date(a.dateAdded);
-  })
+  const [filter, setFilter] = useState('dateAdded');
+
+  if (filter === 'dateAdded') {
+    books.sort(function(a,b) {
+      return new Date(b.dateAdded) - new Date(a.dateAdded);
+    })
+  } else if (filter === 'title') {
+    books.sort(function(a,b) {
+      if(a.title < b.title) {return -1;}
+      if(a.title > b.title) {return 1;}
+      return 0;
+    })
+  }
+
   let body = <></>
   if (books.length === 0) {
     body = <div>
@@ -27,6 +38,17 @@ const DNF = ({books, goToBookPage}) => {
   return (
     <>
       <h1>Did Not Finish</h1>
+      <div className="sort-by">
+        <label htmlFor="sort-by-select"><p>Sort By</p></label>
+        <select
+          name="sort-by"
+          id="sort-by-select"
+          onChange={() => setFilter(event.target.value)}
+        >
+          <option value="dateAdded">Date Added</option>
+          <option value="title">Title</option>
+        </select>
+      </div>
       <div className="tbr">
         {body}
       </div>
--- client/src/components/TBR.jsx
+++ client/src/components/TBR.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import BookCard from './BookCard.jsx';
 
 const TBR = ({books, goToBookPage}) => {
-  books.sort(function(a,b) {
-    return new Date(b.dateAdded) - new Date(a.dateAdded);
-  })
+  const [filter, setFilter] = useState('dateAdded');
+  if (filter === 'dateAdded') {
+    books.sort(function(a,b) {
+      return new Date(b.dateAdded) - new Date(a.dateAdded);
+    })
+  } else if (filter === 'title') {
+    books.sort(function(a,b) {
+      if(a.title < b.title) {return -1;}
+      if(a.title > b.title) {return 1;}
+      return 0;
+    })
+  } else if (filter === 'rating') {
+    books.sort(function(a,b) {
+      if(a.rating < b.rating) {return -1;}
+      if(a.rating > b.rating) {return 1;}
+      return 0;
+    })
+  }
 
   let body = <></>
   if (books.length === 0) {
@@ -28,6 +43,17 @@ const TBR = ({books, goToBookPage}) => {
   return (
     <>
       <h1>To Be Read</h1>
+      <div className="sort-by">
+        <label htmlFor="sort-by-select"><p>Sort By</p></label>
+        <select
+          name="sort-by"
+          id="sort-by-select"
+          onChange={() => setFilter(event.target.value)}
+        >
+          <option value="dateAdded">Date Added</option>
+          <option value="title">Title</option>
+        </select>
+      </div>
       <div className="tbr">
         {body}
       </div>
