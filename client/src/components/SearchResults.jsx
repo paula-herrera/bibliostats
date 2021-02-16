89660ac31c254f7a0cce38d4d6d13bce6c5ec715
Paula Herrera <herrera.paula5@gmail.com>
2021-02-15 21:47:38 -0700

    Added disabled button for books already on shelf

--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -1,20 +1,29 @@
 import React from 'react';
 import BookFromSearch from './BookFromSearch.jsx';
 
-const SearchResults = ({searchResults, addBook}) => {
+const SearchResults = ({searchResults, addBook, allBooks}) => {
   let view = <></>
   if (searchResults.length === 0) {
     view = <div className="no-results">Search For Books</div>
-  } else if (searchResults.length > 0) {
+  } else {
     view =
       <div className="results">
-        {searchResults.map((book, i) =>
-          <BookFromSearch
+        {searchResults.map((book, i) => {
+          let onShelf = false;
+          for (var shelfBook of allBooks) {
+            if (book.id === shelfBook.bookId) {
+              onShelf = true;
+            }
+          }
+          return (
+            <BookFromSearch
             book={book}
             key={i}
             addBook={addBook}
+            onShelf={onShelf}
           />
-        )}
+          )
+          })}
       </div>
   }
 
