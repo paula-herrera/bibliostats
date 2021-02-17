7514c71b19f716712ae84bcd884fa707483ebcf2
Paula Herrera <herrera.paula5@gmail.com>
2021-02-16 19:36:45 -0700

    added heroku url to api requests

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -29,14 +29,14 @@ const App = () => {
   }
 
   const getAllBooks = () => {
-    axios.get('http://localhost:1313/api/getBooks')
+    axios.get('https://git.heroku.com/bibliostats.git/api/getBooks')
       .then(results => {
         setAllBooks(results.data);
       })
   }
 
   const searchBooks = (query) => {
-    axios.get(`http://localhost:1313/api/search/${query}`)
+    axios.get(`https://git.heroku.com/bibliostats.git/api/search/${query}`)
       .then(results => setSearchResults(results.data))
       .catch(() => console.log('Coult not perform search'))
   }
@@ -57,7 +57,7 @@ const App = () => {
         };
       }
     }
-    axios.post('http://localhost:1313/api/addToShelf', bookToAdd)
+    axios.post('//https://git.heroku.com/bibliostats.git/api/addToShelf', bookToAdd)
       .then(() => getAllBooks())
   }
 
@@ -69,7 +69,7 @@ const App = () => {
       format: format,
       rating: rating
     }
-    axios.patch(`http://localhost:1313/api/editBookDetails/${id}`, update)
+    axios.patch(`https://git.heroku.com/bibliostats.git/api/editBookDetails/${id}`, update)
       .then((book) => setSelectedBook(book.data[0]))
       .then(() => getAllBooks())
   }
@@ -78,7 +78,7 @@ const App = () => {
     const update = {
       review: review,
     }
-    axios.patch(`http://localhost:1313/api/editBookReview/${id}`, update)
+    axios.patch(`https://git.heroku.com/bibliostats.git/api/editBookReview/${id}`, update)
       .then((book) => setSelectedBook(book.data[0]))
       .then(() => getAllBooks())
   }
@@ -87,13 +87,13 @@ const App = () => {
     const update = {
       notes: notes,
     }
-    axios.patch(`http://localhost:1313/api/editBookNotes/${id}`, update)
+    axios.patch(`https://git.heroku.com/bibliostats.git/api/editBookNotes/${id}`, update)
       .then((book) => setSelectedBook(book.data[0]))
       .then(() => getAllBooks())
   }
 
   const deleteBook = (id) => {
-    axios.delete(`http://localhost:1313/api/deleteBook/${id}`)
+    axios.delete(`https://git.heroku.com/bibliostats.git/api/deleteBook/${id}`)
       .then(() => setActiveView(prevView))
       .then(() => getAllBooks())
   }
